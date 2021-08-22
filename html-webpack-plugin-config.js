e52459e4614c45739045cf2e5084f4a2f45e29ff
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-08-22 23:43:31 +0900

    共通のレイアウトファイルにカスタマイズ用の変数を導入する

--- html-webpack-plugin-config.js
+++ html-webpack-plugin-config.js
@@ -2,12 +2,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const htmlWebpackPluginOptions = {
   index: {
-    title: "動作テスト",
     filename: "index.html",
     template: "src/template/index.pug",
   },
   access: {
-    title: "ACCESS",
     filename: "access.html",
     template: "src/template/access.pug",
   },
