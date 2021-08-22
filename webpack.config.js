b90114d8e9727171a090ca327486580202e3781d
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-08-22 14:13:03 +0900

    clean-webpack-plugin を設定して dist 下を毎回削除する

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
-const path = require('path');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const cleanWebpackPluginOptions = {
+  cleanOnceBeforeBuildPatterns: [
+    "**/*",
+    "!index.html",
+  ],
+}
 
 const WebpackConfig = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist")
-  }
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js"
+  },
+  plugins: [
+    new CleanWebpackPlugin(cleanWebpackPluginOptions)
+  ]
 }
 
 module.exports = WebpackConfig;
